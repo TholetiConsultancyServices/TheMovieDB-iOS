5865a1044169904830e1f46a35c2768570a68573
Appaji Tholeti <appajit@gmail.com>
2018-11-02 13:54:26 +0000

    Initial Commit

---
+++ TheMovieDB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by Appaji Tholeti on 11/2/18.
+//  Copyright © 2018 Appaji Tholeti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
